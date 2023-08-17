4624b5b69aa3a10e7ef7fdbf3ada8b021bdfed93
ttiurt <truittzach33@gmail.com>
2023-08-17 02:59:43 -0400

    add some responsiveness

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
       <h1>GAMUT</h1>
       <div className={styles.body}>
         
-        <p className={styles.bodyTXT}><span className={styles.abt}>ABOUT</span> &nbsp;<span className={styles.keyWords}>Gamut</span> is a <span className={styles.keyWords}>creative practice</span> rooted in layered narratives and built on partnerships, pulling in collaborators who are the best in their class. We partner with innovative brands and individuals by developing <span className={styles.keyWords}>holistic brands</span> through strategy, design and creative direction. <span className={styles.eml}>HELLO@GAMUTSTUDIO.CO</span></p>
+        <p className={styles.bodyTXT}><span className={styles.abt}>ABOUT</span> <span className={styles.keyWords}>Gamut</span> is a <span className={styles.keyWords}>creative practice</span> rooted in layered narratives and built on partnerships, pulling in collaborators who are the best in their class. We partner with innovative brands and individuals by developing <span className={styles.keyWords}>holistic brands</span> through strategy, design and creative direction. &nbsp; &nbsp;&nbsp;<span className={styles.eml}>HELLO@GAMUTSTUDIO.CO</span></p>
       </div>
       
     </main>
