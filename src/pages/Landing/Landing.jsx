a5fea638263baa13b698a09f579d5dd309dee675
ttiurt <truittzach33@gmail.com>
2023-08-16 21:09:31 -0400

    LP1

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -6,7 +6,10 @@ const Landing = () => {
     <main className={styles.container}>
       <h1>GAMUT</h1>
       <br />
-      <p>Gamut is a creative practice rooted in layered narratives and built on partnerships, pulling in collaborators who are the best in their class. We partner with innovative brands and individuals by developing holistic brands through strategy, design and creative direction.</p>
+      <div className={styles.body}>
+        <p className={styles.bodyTXT}><span className={styles.abt}>ABOUT</span> &nbsp; <span className={styles.keyWords}>Gamut</span> is a <span className={styles.keyWords}>creative practice</span> rooted in layered narratives and built on partnerships, pulling in collaborators who are the best in their class. We partner with innovative brands and individuals by developing <span className={styles.keyWords}>holistic brands</span> through strategy, design and creative direction. &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<span className={styles.eml}>HELLO@GAMUTSTUDIO.CO</span></p>
+      </div>
+      
     </main>
   )
 }
