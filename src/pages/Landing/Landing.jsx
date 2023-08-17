7bdb461aa71084e8fc9914bbf234aa0582f1b696
ttiurt <truittzach33@gmail.com>
2023-08-16 22:24:12 -0400

    add LP changes

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,9 +5,9 @@ const Landing = () => {
   return (
     <main className={styles.container}>
       <h1>GAMUT</h1>
-      <br />
       <div className={styles.body}>
-        <p className={styles.bodyTXT}><span className={styles.abt}>ABOUT</span> &nbsp; <span className={styles.keyWords}>Gamut</span> is a <span className={styles.keyWords}>creative practice</span> rooted in layered narratives and built on partnerships, pulling in collaborators who are the best in their class. We partner with innovative brands and individuals by developing <span className={styles.keyWords}>holistic brands</span> through strategy, design and creative direction. &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<span className={styles.eml}>HELLO@GAMUTSTUDIO.CO</span></p>
+        
+        <p className={styles.bodyTXT}><span className={styles.abt}>ABOUT</span> &nbsp;<span className={styles.keyWords}>Gamut</span> is a <span className={styles.keyWords}>creative practice</span> rooted in layered narratives and built on partnerships, pulling in collaborators who are the best in their class. We partner with innovative brands and individuals by developing <span className={styles.keyWords}>holistic brands</span> through strategy, design and creative direction. <span className={styles.eml}>HELLO@GAMUTSTUDIO.CO</span></p>
       </div>
       
     </main>
